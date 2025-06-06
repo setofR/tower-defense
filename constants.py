780c48706356ba194e6f738d3fec21eaad843a1a
Rohan <35273864+setofR@users.noreply.github.com>
2025-06-06 11:51:42 +0100

    update

--- constants.py
+++ constants.py
@@ -3,5 +3,5 @@ import pygame
 GAME_NAME = "StarGuard"
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
-GAME_LOGO = pygame.image.load('assets/images/starguard-logo.jpg')
+GAME_LOGO = pygame.image.load('assets/images/starguard-logo.png')
 FPS = 60
